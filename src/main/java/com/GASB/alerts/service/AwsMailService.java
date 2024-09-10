ba50e84fddc8df1edc44813026f059c57a40f90c
Yubin <qls773322@naver.com>
2024-09-10 16:24:40 +0900

    KAN-456 <feat: RabbitMQ Test for email service>

--- src/main/java/com/GASB/alerts/service/AwsMailService.java
+++ src/main/java/com/GASB/alerts/service/AwsMailService.java
@@ -1,10 +1,11 @@
 package com.GASB.alerts.service;
 
+import com.GASB.alerts.exception.AlertSettingsNotFoundException;
 import com.GASB.alerts.model.entity.AlertSettings;
-import com.GASB.alerts.repository.AdminUsersRepo;
-import com.GASB.alerts.repository.AlertEmailsRepo;
-import com.GASB.alerts.repository.AlertSettingsRepo;
-import com.GASB.alerts.repository.FileUploadRepo;
+import com.GASB.alerts.model.entity.FileUpload;
+import com.GASB.alerts.model.entity.StoredFile;
+import com.GASB.alerts.model.entity.VtReport;
+import com.GASB.alerts.repository.*;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.model.SendRawEmailRequest;
 import lombok.AllArgsConstructor;
@@ -13,6 +14,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -22,12 +24,51 @@ public class AwsMailService{
     private final AmazonSimpleEmailService amazonSimpleEmailService;
     private final AlertSettingsRepo alertSettingsRepo;
     private final FileUploadRepo fileUploadRepo;
+    private final StoredFileRepo storedFileRepo;
     private final AlertEmailsRepo alertEmailsRepo;
 
     @RabbitListener(queues = "#{@rabbitMQProperties.vtQueue}")
-    public void send(long uploadId) {
-        Long orgId = fileUploadRepo.findOrgIdByUploadId(uploadId);
+    public void send(String payload) {
+        long uploadId = Long.parseLong(payload.trim());
+        System.out.println("uploadId: "+ uploadId);
+        Long orgId = getOrgIdByUploadId(uploadId);
         List<AlertSettings> alertSettings = alertSettingsRepo.findAllByOrgIdAndVtTrue(orgId);
+
+        // storedFile에 이미 존재하면서 vtReport에서 악성으로 판별난 경우
+        if (isExistedStoredFile(uploadId) && isMalware(uploadId)) {
+            sendMail(alertSettings);
+        } else {
+            System.out.println("전혀 위험하지 않음");
+        }
+    }
+
+    private Long getOrgIdByUploadId(long uploadId) {
+        return Optional.ofNullable(fileUploadRepo.findOrgIdByUploadId(uploadId))
+                .orElseThrow(() -> new AlertSettingsNotFoundException("Organization ID not found for uploadId: " + uploadId));
+    }
+
+    private boolean isExistedStoredFile(long uploadId) {
+        FileUpload fileUpload = getFileUploadById(uploadId);
+        return storedFileRepo.existsBySaltedHash(fileUpload.getHash());
+    }
+
+    private boolean isMalware(long uploadId) {
+        StoredFile storedFile = getStoredFileByUploadId(uploadId);
+        VtReport vtReport = storedFile.getVtReport();
+        return vtReport != null && !"none".equals(vtReport.getThreatLabel());
+    }
+
+    private FileUpload getFileUploadById(long uploadId) {
+        return fileUploadRepo.findById(uploadId)
+                .orElseThrow(() -> new AlertSettingsNotFoundException("FileUpload not found for id: " + uploadId));
+    }
+
+    private StoredFile getStoredFileByUploadId(long uploadId) {
+        return Optional.ofNullable(getFileUploadById(uploadId).getStoredFile())
+                .orElseThrow(() -> new AlertSettingsNotFoundException("StoredFile not found for uploadId: " + uploadId));
+    }
+
+    private void sendMail(List<AlertSettings> alertSettings){
         try {
             for(AlertSettings a : alertSettings) {
                 List<String> receivers = alertEmailsRepo.findEmailByAlertId(a.getId());
@@ -35,8 +76,7 @@ public class AwsMailService{
                 amazonSimpleEmailService.sendRawEmail(sendRawEmailRequest);
             }
         }catch (Exception e){
-            log.info("Email Failed");
-            log.info("Error message: " + e.getMessage());
+            log.info("Email Failed message: " + e.getMessage());
         }
     }
 }
